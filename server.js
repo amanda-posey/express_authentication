12de2d365f27227e204bd46c7a6acb11d1294607
amanda=posey <amanda.posey@live.com>
2021-05-13 22:59:39 -0400

    profile: add route and send data to view page

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/profile', (req, res) => {
 
 app.use('/auth', require('./controllers/auth'));
 
+// Add this below /auth controllers
+app.get('/profile', isLoggedIn, (req, res) => {
+  const { id, name, email } = req.user.get(); 
+  res.render('profile', { id, name, email });
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
